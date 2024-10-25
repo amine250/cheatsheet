fccaf3eb006ca2f0d49abe568b447770b2eb024b
Amine <7514608+amine250@users.noreply.github.com>
2024-10-25 15:58:14 +0000

    prepare to merge

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ITCheatSheet from '../components/it-cheat-sheet'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <ITCheatSheet />
+    </main>
+  )
+}
